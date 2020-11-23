d54feb45f569172a1a11d3f8813b7235d6480415
José Murillo <jooseemurillo@gmail.com>
2020-11-23 20:36:46 -0300

    💄 Create page home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Image } from 'react-native';
+
+import { Container, Button, ButtonText } from './styles';
+
+import top from '../../../assets/top.png';
+import back from '../../../assets/back.png';
+import bookLove from '../../../assets/book-love.png';
+import { useNavigation } from '@react-navigation/native';
+
+export default function Home() {
+  
+  const navigation = useNavigation();
+
+
+  return (
+    <Container>
+      <StatusBar style="auto" />
+      <Image source={top} style={{ position: 'absolute', top: 0, left: 0 }} />
+      <Image source={bookLove} />
+
+      <Button 
+        type="login"
+        onPress={() => navigation.navigate('Login')}
+      >
+        <ButtonText type="login">Entrar</ButtonText>
+      </Button>
+
+      <Button 
+        type="register"
+        onPress={() => navigation.navigate('Register')}
+      >
+        <ButtonText type="register">Cadastro</ButtonText>
+      </Button>
+
+      <Image source={back} style={{ position: 'absolute', bottom: 0, right: 0 }} />
+    </Container>
+  );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,50 @@
+import styled, { css } from 'styled-components/native';
+
+interface IButtonText {
+  type: string;
+}
+
+interface IButton {
+  type: string;
+}
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Button = styled.TouchableOpacity<IButton>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+
+  ${props => props.type === 'login' ? css`
+    background: #231FE4;
+    margin-top: 50px;
+  ` : css`
+    background: transparent;
+    margin-top: 20px;
+  `}
+
+  border: 2px solid #231FE4;
+  border-radius: 31px;
+
+  width: 193px;
+  height: 44px;
+`;
+
+
+export const ButtonText = styled.Text<IButtonText>`
+
+  ${props => props.type === 'login' ? css`
+    color: #FFFFFF;
+  ` : css`
+    color: #161494;
+  `}
+
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 20px;
+`;
